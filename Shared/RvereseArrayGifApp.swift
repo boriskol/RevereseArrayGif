f166e996d7d17220af4766141cc3b4beab8d16d2
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-02-27 05:54:52 +0100

    Initial Commit

---
+++ Shared/RvereseArrayGifApp.swift
@@ -0,0 +1,17 @@
+//
+//  RvereseArrayGifApp.swift
+//  Shared
+//
+//  Created by Borna Libertines on 27/02/22.
+//
+
+import SwiftUI
+
+@main
+struct RvereseArrayGifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
